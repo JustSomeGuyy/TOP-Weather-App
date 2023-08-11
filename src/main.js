99b5879131cb5992ca56cedcd8024a457b5e0e7f
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2023-08-11 00:22:59 -0500

    finished rough css for layout/design. Added function for current local time that updates every second.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-const apiUrl = 'http://api.weatherapi.com/v1/forecast.json?key=63d73608c67941668ae115410233107&';
+const apiUrl = 'http://api.weatherapi.com/v1/forecast.json?';
+const url = config.MY_Key;
+
+// This is for theD DOM elements.
+const weekday = document.getElementById('day-of-week');
+const date = document.getElementById('date');
 
 async function localWeather(position) {
-  const geoWeather = await fetch(`${apiUrl}q=${position.coords.latitude},${position.coords.longitude}&days=3`, { mode: 'cors' });
+  const geoWeather = await fetch(`${apiUrl}&key=${url}q=${position.coords.latitude},${position.coords.longitude}&days=3`, { mode: 'cors' });
   const data = await geoWeather.json();
   console.log(data);
   populateCityName(data);
+  currentConditions(data);
 }
 
 function getLocation() {
@@ -17,9 +23,49 @@ function getLocation() {
 
 function populateCityName(obj) {
   const cityName = document.getElementById('location-name');
-  const countryName = document.getElementById('country-name')
+  const countryName = document.getElementById('country-name');
   cityName.innerText = `${obj.location.name}, ${obj.location.region}`;
   countryName.innerText = obj.location.country;
 }
 
 getLocation();
+
+function currentConditions(param) {
+  const temp = document.getElementById('current-weather');
+  const conditions = document.getElementById('weather-conds');
+  const currentTemp = param.current.temp_c;
+  const roundedTemp = Math.floor(currentTemp);
+  temp.innerHTML = `${roundedTemp}<span class='degree'>&deg;</span>`;
+  conditions.innerText = param.current.condition.text;
+}
+
+function getTime() {
+  const getCurrentTime = new Date();
+  const displayTime = document.getElementById('time');
+  const hours = getCurrentTime.getHours();
+  const minutes = getCurrentTime.getMinutes();
+  let minute;
+  let hour;
+  let amOrPm;
+  if (hours === 0 && hours < 1) {
+    amOrPm = 'AM';
+    hour = hours + 12;
+  } else if (hours > 12 && hours > 0) {
+    amOrPm = 'AM';
+    hour = hours;
+  } else {
+    amOrPm = 'PM';
+    hour = hours - 12;
+  };
+  if (minutes < 10) {
+    minute = `0${minutes}`;
+  } else {
+    minute = minutes;
+  };
+  displayTime.innerText = `${hour} : ${minute} ${amOrPm}`;
+}
+
+function updateTimeAndDate() {
+  setInterval(getTime, 1000);
+}
+updateTimeAndDate();
