b7e37eb6ffaca357185d642b0f437f0763805d80
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2023-08-23 05:35:43 -0500

    Fixed bug with duplicating data.

--- src/main.js
+++ src/main.js
@@ -30,6 +30,7 @@ updateTimeAndDate();
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(localWeather);
+    
   } else {
     // x.innerHTML = 'Geolocation is not supported by this browser';
   }
@@ -63,41 +64,39 @@ function displayCurrentDetails() {
   }
 }
 
-displayCurrentDetails()
+displayCurrentDetails();
+
+// This is for the card that displays the high and low temps. //
+const highestTemp = document.createElement('p');
+const lowestTemp = document.createElement('p');
 
 function addTempDetails(param) {
-  const highestTemp = document.createElement('p');
-  const lowestTemp = document.createElement('p');
   const temps = document.getElementById('0');
-  highestTemp.innerHTML;
-  lowestTemp.innerHTML;
   temps.append(highestTemp, lowestTemp);
   highestTemp.innerHTML = `High: ${Math.floor(param.forecast.forecastday[0].day.maxtemp_c)}<span>&deg;</span>c`;
   lowestTemp.innerHTML = `Low: ${Math.floor(param.forecast.forecastday[0].day.mintemp_c)}<span>&deg;</span>c`;
 }
 
+// This is for the wind details card //
+const windNormal = document.createElement('p');
+const windGust = document.createElement('p');
+const windDirection = document.createElement('p');
+
 function addWindDetails(param) {
-  const windNormal = document.createElement('p');
-  const windGust = document.createElement('p');
-  const windDirection = document.createElement('p');
   const wind = document.getElementById('1');
-  windNormal.innerText;
-  windGust.innerText;
-  windDirection.innerText;
   wind.append(windNormal, windDirection, windGust);
   windNormal.innerText = `Wind: ${Math.floor(param.current.wind_kph)} kph`;
   windDirection.innerText = param.current.wind_dir;
   windGust.innerText = `Gusting: ${Math.floor(param.current.gust_kph)} kph`;
 }
 
+// This is for the card that displays further details, such as humidity, visibility, and UV scale.
+const ultraViolet = document.createElement('p');
+const visibility = document.createElement('p');
+const humidity = document.createElement('p');
+
 function addFurtherDetails(param) {
   const detailsDisplay = document.getElementById('2');
-  const ultraViolet = document.createElement('p');
-  const visibility = document.createElement('p');
-  const humidity = document.createElement('p');
-  visibility.innerText;
-  ultraViolet.innerText;
-  humidity.innerText;
   detailsDisplay.append(ultraViolet, visibility, humidity);
   ultraViolet.innerText = `UV Index: ${param.current.uv}`;
   visibility.innerText = `Visibility: ${param.current.vis_km} km`;
@@ -111,39 +110,33 @@ function addCurrentDetails(param) {
   addWindDetails(param);
 }
 
-async function localWeather(position) {
-  const geoWeather = await fetch(`${apiUrl}&key=${url}q=${position.coords.latitude},${position.coords.longitude}&days=4`, { mode: 'cors' });
-  const data = await geoWeather.json();
-  console.log(data);
-  populateCityName(data);
-  addCurrentDetails(data);
-  addHourlyWeather(data);
-  buildingForecastDisplay(data);
-}
+// This is for the hourly weather creation //
 
-async function addHourlyWeather(param) {
-  const hourlyWeather = document.getElementById('hourly-weather');
-  const timeReference = new Date().getHours();
-  const hours = ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'];
+const hourlyWeather = document.getElementById('hourly-weather');
+const timeReference = getCurrentDate.getHours();
+const hours = ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'];
+let hourlyWeatherHours;
+let nextDayHourlyWeather;
+
+function addHourlyWeather(param) {
   const hourlyWeatherUpdates = param.forecast.forecastday[0].hour;
-  const nextDayWeather = param.forecast.forecastday[1].hour;
   if (timeReference <= hourlyWeatherUpdates.length && timeReference <= hours.length) {
     const matchedWeatherHours = hourlyWeatherUpdates.slice(timeReference);
     const hoursShort = hours.slice(timeReference);
     for (let i = 0; i < matchedWeatherHours.length; i += 1) {
-      const weatherHours = document.createElement('li');
-      weatherHours.innerHTML = `${hoursShort[i]}: ${Math.floor(matchedWeatherHours[i].temp_c)}<span>&deg;</span>c`;
-      hourlyWeather.append(weatherHours);
+      hourlyWeatherHours = document.createElement('li');
+      hourlyWeatherHours.innerHTML = `${hoursShort[i]}: ${Math.floor(matchedWeatherHours[i].temp_c)}<span>&deg;</span>c`;
+      hourlyWeather.append(hourlyWeatherHours);
     }
   }
-  for (let o = 0; hours.length > 0; o += 1) {
-    const nextDay = document.createElement('li');
-    nextDay.innerHTML = `${hours[o]}: ${Math.floor(nextDayWeather[o].temp_c)}<span>&deg;</span>c`;
-    hourlyWeather.append(nextDay);
+  for (let o = 0; hours.length > o; o += 1) {
+    nextDayHourlyWeather = document.createElement('li');
+    nextDayHourlyWeather.innerHTML = `${hours[o]}: ${Math.floor(param.forecast.forecastday[1].hour[o].temp_c)}<span>&deg;</span>c`;
+    hourlyWeather.append(nextDayHourlyWeather);
   }
 }
 
-// This is for displaying the date in the forecast cards
+// This is for displaying the date in the forecast cards //
 const futureDate = new Date(getCurrentDate);
 
 function futureDates(date, num) {
@@ -154,11 +147,10 @@ function futureDates(date, num) {
 
 const oneDay = futureDates(futureDate, 1);
 const twoDay = futureDates(futureDate, 1);
-const threeDay = futureDates(futureDate, 1);
 
-// This is for building the cards for displaying the forecasted weather
+// This is for building the cards for displaying the forecasted weather //
 const displayCards = document.getElementById('forecast');
-for (let i = 0; i <= 2; i += 1) {
+for (let i = 0; i <= 1; i += 1) {
   const cards = document.createElement('div');
   const forecastDate = document.createElement('p');
   const highLowTemp = document.createElement('p');
@@ -171,26 +163,19 @@ for (let i = 0; i <= 2; i += 1) {
 }
 
 const forecastDateOne = document.getElementById('dates-0');
-const forecastdateTwo = document.getElementById('dates-1');
-const forecastDateThree = document.getElementById('dates-2');
+const forecastDateTwo = document.getElementById('dates-1');
 
 forecastDateOne.innerText = oneDay;
-forecastdateTwo.innerText = twoDay;
-forecastDateThree.innerText = threeDay;
+forecastDateTwo.innerText = twoDay;
 
 function displayForecast(param) {
   const forecastOne = document.getElementById('box-0');
   const forecastTwo = document.getElementById('box-1');
-  const forecastThree = document.getElementById('box-2');
   const highForecastTemp = [];
   const lowForecastTemp = [];
   const tempsOne = document.getElementById('temps-0');
   const tempsTwo = document.getElementById('temps-1');
-  const tempsThree = document.getElementById('temps-2');
-  tempsOne.innerHTML;
-  tempsTwo.innerHTML;
-  tempsThree.innerHTML;
-  for (let i = 1; i <= 3; i += 1) {
+  for (let i = 1; i <= 2; i += 1) {
     const highTemp = Math.floor(param.forecast.forecastday[i].day.maxtemp_c);
     const lowTemp = Math.floor(param.forecast.forecastday[i].day.mintemp_c);
     highForecastTemp.push(highTemp);
@@ -198,26 +183,35 @@ function displayForecast(param) {
   }
   tempsOne.innerHTML = `High: ${highForecastTemp[0]}<span>&deg;</span>c Low: ${lowForecastTemp[0]}<span>&deg;</span>c`;
   tempsTwo.innerHTML = `High: ${highForecastTemp[1]}<span>&deg;</span>c Low: ${lowForecastTemp[1]}<span>&deg;</span>c`;
-  tempsThree.innerHTML = `High: ${highForecastTemp[2]}<span>&deg;</span>c  Low: ${lowForecastTemp[2]}<span>&deg;</span>c`;
   forecastOne.appendChild(tempsOne);
   forecastTwo.appendChild(tempsTwo);
-  forecastThree.appendChild(tempsThree);
 }
 
-function buildingForecastDisplay(param) {
-  displayForecast(param);
+async function localWeather(position) {
+  const geoWeather = await fetch(`${apiUrl}&key=${url}q=${position.coords.latitude},${position.coords.longitude}&days=4`, { mode: 'cors' });
+  const data = await geoWeather.json();
+  console.log(data);
+  populateCityName(data);
+  addCurrentDetails(data);
+  addHourlyWeather(data);
+  displayForecast(data);
 }
+
+// TO DO TOMORROW AUGUST 21 ST
+// Fix bug with duping displaying data. Hourly and details weather are duplicating.
 
 const locationSearch = document.getElementById('location-search');
 
 async function locationLookUp() {
   const location = locationSearch.value;
-  const geoWeather = await fetch(`${apiUrl}&key=${url}q=${location}&days=4`, { mode: 'cors' });
+  const geoWeather = await fetch(`${apiUrl}&key=${url}q=${location}&days=3`, { mode: 'cors' });
   const data = await geoWeather.json();
+  console.log(data);
+  clearElements();
   populateCityName(data);
   addCurrentDetails(data);
-  addHourlyWeather(data);
-  buildingForecastDisplay(data);
+  newLocationHourlyWeather(data);
+  displayForecast(data);
 }
 
 locationSearch.addEventListener('keydown', (e) => {
@@ -225,3 +219,25 @@ locationSearch.addEventListener('keydown', (e) => {
     locationLookUp();
   }
 });
+
+function clearElements() {
+  hourlyWeather.innerHTML = '';
+}
+
+function newLocationHourlyWeather(param) {
+  const hourlyWeatherUpdates = param.forecast.forecastday[0].hour;
+  if (timeReference <= hourlyWeatherUpdates.length && timeReference <= hours.length) {
+    const matchedWeatherHours = hourlyWeatherUpdates.slice(timeReference);
+    const hoursShort = hours.slice(timeReference);
+    for (let i = 0; i < matchedWeatherHours.length; i += 1) {
+      hourlyWeatherHours = document.createElement('li');
+      hourlyWeatherHours.innerHTML = `${hoursShort[i]}: ${Math.floor(matchedWeatherHours[i].temp_c)}<span>&deg;</span>c`;
+      hourlyWeather.append(hourlyWeatherHours);
+    }
+  }
+  for (let o = 0; hours.length > o; o += 1) {
+    nextDayHourlyWeather = document.createElement('li');
+    nextDayHourlyWeather.innerHTML = `${hours[o]}: ${Math.floor(param.forecast.forecastday[1].hour[o].temp_c)}<span>&deg;</span>c`;
+    hourlyWeather.append(nextDayHourlyWeather);
+  }
+}
